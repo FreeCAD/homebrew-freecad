5feb0f2921b7755d9ca5740ac38bfdf0ed88606c
Bruce B. Lacey <bruce@blacey.com>
2016-10-03 10:10:47 -0700

    Refactored freecad formula for version 0.16+
      - Set url resource to GitHub release 0.16
      - Depends on OCCT7 instead of OCE (deprecated)
      - Formula minimum OS is Mavericks+ (homebrew and travis require Yosemite+)
      - Added new vtk, nglib, med-file dependencies - fully qualified to use associated bottles
      - Simplified formula logic given FreeCAD cmake robustness improvements since 0.15
    
    Currently --only-dependencies has been tested because this is the minimum requirement for
    travis-ci builds
    
    References #24

--- freecad.rb
+++ freecad.rb
@@ -1,111 +1,57 @@
-require "formula"
-
 class Freecad < Formula
-  homepage "http://sourceforge.net/projects/free-cad/"
-  head "git://git.code.sf.net/p/free-cad/code"
-  url "http://downloads.sourceforge.net/project/free-cad/FreeCAD%20Source/freecad_0.15.4671.tar.gz"
-  sha256 "8dda8f355cb59866a55c9c6096f39a3ebc5347892284db4c305352cc9be03bbc"
+  homepage "http://www.freecadweb.org"
+  url "https://github.com/FreeCAD/FreeCAD/archive/0.16.tar.gz"
+  version "0.16"
+  sha256 "6cc71ab4b0dc60b493d3aaa4b42f1ce1af9d4fcd539309ab0792804579e18e09"
+  head "https://github.com/FreeCAD/FreeCAD.git", :branch => "master"
 
   # Debugging Support
   option 'with-debug', 'Enable debugging build'
 
-  # Should work with OCE (OpenCascade Community Edition) or Open Cascade
-  # OCE is the prefered option
-  option 'with-opencascade', 'Build with OpenCascade'
-
   # Build without external pivy (use old bundled version)
   option 'without-external-pivy', 'Build without external Pivy (use old bundled version)'
   
-  occ_options = []
-  if MacOS.version < 10.7
-    occ_options = ['--without-tbb']
-  end
-  
-  if build.with? 'opencascade'
-    depends_on 'opencascade' => occ_options
-  else
-    depends_on 'oce' => occ_options
-  end
-
   # Build dependencies
+  depends_on 'cmake'   => :build
+  depends_on 'ccache'  => :build
   depends_on 'doxygen' => :build
-  depends_on 'cmake' => :build
-  depends_on :fortran => :build
 
   # Required dependencies
-  depends_on 'boost'
-  depends_on 'sip'
-  depends_on 'xerces-c'
+  depends_on :macos => :mavericks
   depends_on 'eigen'
-  depends_on 'coin' => ['--without-framework', '--without-soqt']
+  depends_on 'freetype'
   depends_on 'qt'
-  depends_on 'pyqt'
-  depends_on 'shiboken'
+  depends_on 'python'
+  depends_on 'boost-python'
   depends_on 'pyside'
   depends_on 'pyside-tools'
-  depends_on 'python'
-  depends_on 'orocos-kdl'
-
-  # Recommended dependencies
-  depends_on 'freetype' => :recommended
-
-  # Allow building with internal pivy
-  unless build.without? 'external-pivy'
-    depends_on 'pivy' => [:recommended, '--HEAD']
-  end
-
-  # Optional Dependencies
-  depends_on :x11 => :optional
+  depends_on 'xerces-c'
+  depends_on 'homebrew/science/opencascade'
+  depends_on 'homebrew/science/orocos-kdl'
+  depends_on 'homebrew/python/matplotlib'
+  depends_on 'FreeCAD/freecad/pivy' unless build.without? 'external-pivy'
+  depends_on 'FreeCAD/freecad/vtk'      #Custom bottle using --without-python
+  depends_on 'FreeCAD/freecad/coin'     #Custom bottle without-soqt and without-framework
+  depends_on 'FreeCAD/freecad/nglib'    #Custom bottle for use with opencascade 7
+  depends_on 'FreeCAD/freecad/med-file'
 
   def install
-    if build.with? 'debug'
-      ohai "Creating debugging build..."
-    end
 
-    # Allow building with internal pivy
-    use_external_pivy='ON'
-    if build.without? 'external-pivy'
-      ohai "Building without external Pivy"
-      use_external_pivy='OFF'
-    end
-
-    # Enable Fortran
-    libgfortran = `$FC --print-file-name libgfortran.a`.chomp
-    ENV.append 'LDFLAGS', "-L#{File.dirname libgfortran} -lgfortran"
-
-    # Brewed python include and lib info
-    # TODO: Don't hardcode bin path
-    python_prefix = `/usr/local/bin/python-config --prefix`.strip
-    python_library = "#{python_prefix}/Python"
-    python_include_dir = "#{python_prefix}/Headers"
-
-    # Find OCE cmake file location
-    # TODO add opencascade support/detection
-    oce_dir = "#{Formula['oce'].opt_prefix}/OCE.framework/Versions/#{Formula['oce'].version}/Resources"
+    system "rm", "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-science/nglib.rb"
 
     # Set up needed cmake args
     args = std_cmake_args + %W[
-      -DFREECAD_USE_EXTERNAL_PIVY=#{use_external_pivy}
+      -DBUILD_FEM_NETGEN:BOOL=ON
       -DFREECAD_USE_EXTERNAL_KDL=ON
-      -DPYTHON_LIBRARY=#{python_library}
-      -DPYTHON_INCLUDE_DIR=#{python_include_dir}
-      -DOCE_DIR=#{oce_dir}
-      -DFREETYPE_INCLUDE_DIRS=#{Formula.factory('freetype').opt_prefix}/include/freetype2/
+      -DCMAKE_USE_EXTERNAL_PIVY:BOOL=#{build.with?('external-pivy') ? 'ON' : 'OFF'}
+      -DCMAKE_BUILD_TYPE=#{build.with?('debug') ? 'Debug' : 'Release'}
     ]
 
-    if build.with? 'debug'
-      # Create debugging build and tack on the build directory
-      args << '-DCMAKE_BUILD_TYPE=Debug' << '.'
-    
-      system "cmake", *args
-      system "make", "install"
-    else
-      # Create standard build and tack on the build directory
-      args << '.'
-    
-      system "cmake", *args
-      system "make", "install/strip"
+    mkdir "Build" do
+       system "cmake", "..", *args
+       system "make", "install"
     end
+
   end
 
   def caveats; <<-EOS.undent
