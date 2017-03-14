e20bc0cc74eda878cb155f411ce03388a207695e
Ian Rees <ian.rees@gmail.com>
2017-03-14 21:16:38 +1300

    Update OpenCAMLib to latest working commit.

--- opencamlib.rb
+++ opencamlib.rb
@@ -3,51 +3,41 @@ class Opencamlib < Formula
   homepage "http://www.anderswallin.net/CAM/"
   version "0.0.1" # TODO Specify a real version here - note usage below
   url "https://github.com/aewallin/opencamlib.git",
-      :revision => "30ed446"
+      :revision => "398f7c7fac60169fda067ea0f5ebc15083361f5c"
   head "https://github.com/aewallin/opencamlib.git", :using => :git
-
-  # OpenCAMLib uses git-fu to set the version string, fix it for Homebrew.
-  patch :DATA
 
   depends_on "cmake" => :build
 
-  #TODO Make OpenMP optional (upstream)
+  depends_on :python => :recommended
 
-  # Need llvm from Homebrew, to get OpenMP support
-  depends_on "llvm" => :build
-
-  option "without-python", "Do not build Python bindings."
+  option "with-openmp", "Build with support for OpenMP parallel processing"
+  depends_on "llvm" => :build if build.with?("openmp")
 
   def install
 
-    if build.with? "python"
-      pyhome = `#{Formula["python"].bin}/python-config --prefix`.chomp
-      # Borrowed this from llvm formula, but had to specify path to
-      # python-config - hopefully that doesn't create issues...
-      pylib = "#{pyhome}/lib/libpython2.7.dylib"
-      pyinclude = "#{pyhome}/include/python2.7"
-    end
-
-    llvm_lib = Formula["llvm"].lib
-    llvm_inc = "#{llvm_lib}/clang/#{Formula["llvm"].version}" << "/include"
-
-    open("src/git-tag.txt", "w") do |f|
-      f << version
+    if build.with? "openmp"
+      llvm_lib = Formula["llvm"].lib
+      llvm_inc = "#{llvm_lib}/clang/#{Formula["llvm"].version}" << "/include"
     end
 
     mkdir "build" do
       cmake_args = std_cmake_args
-      cmake_args << "-DCMAKE_C_COMPILER=#{Formula["llvm"].bin}/clang"
-      cmake_args << "-DCMAKE_CXX_COMPILER=#{Formula["llvm"].bin}/clang++"
+      if build.with? "openmp"
+        cmake_args << "-DCMAKE_C_COMPILER=#{Formula["llvm"].bin}/clang"
+        cmake_args << "-DCMAKE_CXX_COMPILER=#{Formula["llvm"].bin}/clang++"
 
-      cmake_args << "-DCMAKE_MODULE_LINKER_FLAGS=-undefined dynamic_lookup -L#{llvm_lib} -Wl,-rpath,#{llvm_lib}"
+        cmake_args << "-DCMAKE_MODULE_LINKER_FLAGS=-undefined dynamic_lookup -L#{llvm_lib} -Wl,-rpath,#{llvm_lib}"
 
-      cmake_args << "-DCMAKE_C_FLAGS=-I#{llvm_inc}"
-      cmake_args << "-DCMAKE_CXX_FLAGS=-I#{llvm_inc} -std=c++11"
+        cmake_args << "-DCMAKE_C_FLAGS=-I#{llvm_inc}"
+        cmake_args << "-DCMAKE_CXX_FLAGS=-I#{llvm_inc} -std=c++11"
+      else
+        cmake_args << "-DUSE_OPENMP=0"
+      end
 
       if build.with? "python"
-        cmake_args << "-DPYTHON_INCLUDE_DIR=#{pyinclude}"
-        cmake_args << "-DPYTHON_LIBRARY=#{pylib}"
+        cmake_args << "-DPYTHON_EXECUTABLE=#{Formula["python"].bin}/python"
+      else
+        cmake_args << "-DBUILD_PY_LIB=0"
       end
 
       system "cmake", *cmake_args, ".."
@@ -55,18 +45,4 @@ class Opencamlib < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/version_string.cmake b/src/version_string.cmake
-index 910a5f7..140f620 100644
---- a/src/version_string.cmake
-+++ b/src/version_string.cmake
-@@ -27,6 +27,7 @@ endif()
- 
- if(GIT_FOUND)
-     execute_process(
-+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-         COMMAND ${GIT_EXECUTABLE} describe --tags 
-         RESULT_VARIABLE res_var 
-         OUTPUT_VARIABLE GIT_COM_ID 
 
