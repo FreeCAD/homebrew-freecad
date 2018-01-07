cd4f7fca1250293a3e7d688dd24c04af8934eca9
Ian Rees <ian.rees@gmail.com>
2018-01-07 14:37:50 +1300

    Remove unnecessary libclang resource

--- Formula/pyside2.rb
+++ Formula/pyside2.rb
@@ -12,26 +12,17 @@ class Pyside2 < Formula
   depends_on "python3" => :optional
 
   depends_on "cmake" => :build
-  # depends_on "llvm" => :build
+  depends_on "llvm" => :build
   depends_on "qt"
-
-  resource "libclang" do
-    url "http://download.qt.io/development_releases/prebuilt/libclang/libclang-release_50-mac.7z"
-    sha256 "fa7e4c27fdebd72131ca364b555bdfda06afa9060de957f7abbc6b1205d3b5cf"
-  end
 
   def install
     ENV.cxx11
-    
-    # libclang unpacks in to a subdirectory called libclang
-    resource("libclang").stage(buildpath)
-    ENV["LLVM_INSTALL_DIR"] = "#{buildpath}/libclang"
 
-    qt = Formula["qt"]
+    ENV["LLVM_INSTALL_DIR"] = "#{Formula["llvm"].opt_prefix}"
 
     Language::Python.each_python(build) do |python, version|
       system python, "setup.py", "build"
-# "--jobs=#{ENV.make_jobs}", 
+      # , "--jobs=#{ENV.make_jobs}"
     end
   end
 
