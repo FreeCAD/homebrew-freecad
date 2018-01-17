3bbaebd547c6168a0b109a51f2b422e8cc6b8961
Ian Rees <ian.rees@gmail.com>
2018-01-17 16:29:49 +1300

    PySide2/Qt5.10 builds with "--HEAD --env=std"

--- Formula/pyside2.rb
+++ Formula/pyside2.rb
@@ -1,6 +1,7 @@
 class Pyside2 < Formula
   desc "Python bindings for Qt"
   homepage "https://wiki.qt.io/PySide2"
+  # TODO: Need to change this so it also gets the submodules, or wait until it's fixed upstream
   url "https://codereview.qt-project.org/gitweb?p=pyside/pyside-setup.git;a=snapshot;h=04af851b4b886675fc68e0f8e637d9e399d4000c;sf=tgz"
   sha256 "46c750e4f67f87b78627f45c4a4f74bd3a418681f6dc66468b140c23b1265965"
   version "2.0.0-04af851"
@@ -15,15 +16,10 @@ class Pyside2 < Formula
   depends_on "qt"
 
   def install
-    llvm_path = Formula["llvm"].opt_prefix
-
-    # The Homebrew clang++ shim causes problems with Shiboken2
-    ENV.prepend_path "PATH", "#{llvm_path}/bin"
-
-    ENV["LLVM_INSTALL_DIR"] = llvm_path
+    ENV["LLVM_INSTALL_DIR"] = Formula["llvm"].opt_prefix
 
     Language::Python.each_python(build) do |python, version|
-      system "python", *Language::Python.setup_install_args(prefix), "--debug"
+      system "python", *Language::Python.setup_install_args(prefix)
     end
   end
 
