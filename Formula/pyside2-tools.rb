b6de8fb90a73ee0eb631bfd81d07ca6a5376a912
Bruce B. Lacey <bruce@blacey.com>
2018-01-26 15:02:02 -0800

    PySide2-tools - Set url to 5.9 413ecc7 (head on 1/25/2018)
      - Head determined using `git submodule status` within
        the pyside2-setup repo

--- Formula/pyside2-tools.rb
+++ Formula/pyside2-tools.rb
@@ -1,9 +1,9 @@
 class Pyside2Tools < Formula
   desc "PySide development tools (pyuic and pyrcc)"
   homepage "https://wiki.qt.io/PySide2"
-  url "https://codereview.qt-project.org/gitweb?p=pyside/pyside-tools.git;a=snapshot;h=5d6b74066e61069358a3331114ab21252d079e69;sf=tgz"
-  sha256 "23ce27ce8fa9fd305592d2ee69737d96249467faab7b1c5b47e4cd2677353761"
-  version "5.9-1"
+  url "https://codereview.qt-project.org/gitweb?p=pyside/pyside-tools.git;a=snapshot;h=413ecc73fbe6d6717ae2132e86648ac8b6da9d3c;sf=tgz"
+  sha256 "f76d9686f963353bbcf82cc147f400f31cfa6140bdbd1196797adce93d052e67"
+  version "5.9-413ecc7"
   # Git commit log 'https://codereview.qt-project.org/gitweb?p=pyside/pyside-tools.git'
 
   head "https://codereview.qt-project.org/pyside/pyside-tools", :branch => "5.9"
