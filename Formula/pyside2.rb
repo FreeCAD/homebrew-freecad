53262f18bd4219a7ee927a66f60de93b4ac6175b
freecad <brew-test-bot@googlegroups.com>
2018-03-28 21:27:13 -0700

    pyside2: add 5.9-b09fde6 bottle.

--- Formula/pyside2.rb
+++ Formula/pyside2.rb
@@ -1,6 +1,13 @@
 class Pyside2 < Formula
   desc "Python bindings for Qt5 and greater"
   homepage "https://wiki.qt.io/PySide2"
+  bottle do
+    root_url "https://dl.bintray.com/freecad/bottles-freecad"
+    sha256 "357ab6aacf87dca1140b5e2d26be5110a58d5c237e52637b3a5dd71a2cc5d3e5" => :high_sierra
+    sha256 "72097faadc56479676a8f83466d43e58242d1a078c181643df51cf539867f35e" => :sierra
+    sha256 "9d9b89ec8799ea248352ac6e00cec06a06e13a962ffafa8ebe42f796ba92fa92" => :el_capitan
+  end
+
    url "https://codereview.qt-project.org/gitweb?p=pyside/pyside-setup.git;a=snapshot;h=b09fde6260b255e8b93b0d20a337e701bc940a99;sf=tgz"
   sha256 "47a1dde02358045bd2441ab470d4d917a6b449daa389b3e5d41c513f50645f2f"
   version "5.9-b09fde6"
