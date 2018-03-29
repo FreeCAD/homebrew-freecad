68d5a29126305e7329fb3b40d57b74204b6855fb
freecad <brew-test-bot@googlegroups.com>
2018-03-28 21:27:13 -0700

    qtwebkit: add 5.212-72cfbd bottle.

--- Formula/qtwebkit.rb
+++ Formula/qtwebkit.rb
@@ -5,6 +5,14 @@ class Qtwebkit < Formula
     :revisoin => '72cfbd7664f21fcc0e62b869a6b01bf73eb5e7da'
   head "https://codereview.qt-project.org/qt/qtwebkit", :using => :git, :branch => '5.212'
   version "5.212-72cfbd"
+  bottle do
+    root_url "https://dl.bintray.com/freecad/bottles-freecad"
+    cellar :any
+    sha256 "5abfe763173ecf201b69766728e8157ebde4ebd48d3de83354c94a559b26741a" => :high_sierra
+    sha256 "58ed0e1ee474bac3d64a1c283e4893e86383eba3f6165f167f9fc15be2ce5ff6" => :sierra
+    sha256 "649b06be005c2c4a37946434164331786af2420beaac5586b2473e59e3f6b4be" => :el_capitan
+  end
+
   sha256 ""
   depends_on "qt"
   depends_on "jpeg"
