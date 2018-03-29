c865be71ab8ed4c3f8e93bf4bdabf38102976ad7
freecad <brew-test-bot@googlegroups.com>
2018-03-28 21:27:13 -0700

    pyside2-tools: add 5.9-413ecc7 bottle.

--- Formula/pyside2-tools.rb
+++ Formula/pyside2-tools.rb
@@ -7,6 +7,14 @@ class Pyside2Tools < Formula
 
   head "https://codereview.qt-project.org/p/pyside/pyside-tools.git", :branch => "5.9"
 
+  bottle do
+    root_url "https://dl.bintray.com/freecad/bottles-freecad"
+    cellar :any
+    sha256 "2d1905bf512f6fa7ad69784e93c80c8193bf9252b8548758e662b8bb2467c406" => :high_sierra
+    sha256 "761cfb1a48df6844675f10ef18856a581b5253c7b72c159c94631bd227b334d9" => :sierra
+    sha256 "a000110b4aae8e486d93631cdc74b258ec6798c210fa4d75ab28ff1ed86e551b" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "FreeCAD/freecad/pyside2"
 
