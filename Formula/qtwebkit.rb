c7389720b53807bdca122d82101e3666994eca94
Peter Lama <peterldev94@gmail.com>
2018-08-11 08:29:41 -0700

    Bump qtwebkit revision for rebuild

--- Formula/qtwebkit.rb
+++ Formula/qtwebkit.rb
@@ -5,16 +5,7 @@ class Qtwebkit < Formula
     :revisoin => '72cfbd7664f21fcc0e62b869a6b01bf73eb5e7da'
   head "https://codereview.qt-project.org/qt/qtwebkit", :using => :git, :branch => '5.212'
   version "5.212-72cfbd"
-  revision 1
-  
-  bottle do
-    root_url "https://dl.bintray.com/freecad/bottles-freecad"
-    cellar :any
-    rebuild 1
-    sha256 "d9a23533540d8a51f60facd04565f06dd9593600f706886e0620aef94859f598" => :high_sierra
-    sha256 "bf38b34f0a1609342ea706f9a9ea28106e86925b83d8b5f0f0945830fe344ce0" => :sierra
-    sha256 "8b411648332adf546509bf52c94b8fab3987487e9dd71c7d5cda02daa8544087" => :el_capitan
-  end
+  revision 2
 
   sha256 ""
   depends_on "qt"
