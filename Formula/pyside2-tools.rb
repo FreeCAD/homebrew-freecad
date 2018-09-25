1829d001e526cd170fbcf2aa1ea3f1a59f36a3d6
peterlama <peterldev94@gmail.com>
2018-09-24 22:04:38 -0700

    pyside2-tools: add 5.11.2 bottle.

--- Formula/pyside2-tools.rb
+++ Formula/pyside2-tools.rb
@@ -5,6 +5,14 @@ class Pyside2Tools < Formula
   version "5.11.2"
   head "http://code.qt.io/pyside/pyside-setup.git", :branch => "5.11" 
 
+  bottle do
+    root_url "https://dl.bintray.com/freecad/bottles-freecad"
+    cellar :any
+    sha256 "324c237a776b12b7f7bf70bb272d3115b4418339a26c581819a3077ec8f21ed0" => :high_sierra
+    sha256 "cae8ea865acc1cbba241afce076c5a7ecdbe6c9aa44cc087d905e63eb62f4f14" => :sierra
+    sha256 "1005008a3680930da64eb7709dee6405ef4061bacdc5fca3e576a4aa7c57591d" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "python@2" => :recommended
   depends_on "python3" => :optional
