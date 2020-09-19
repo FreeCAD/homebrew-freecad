cfaf720d877fe7adf2d0c75fb28f60eb9ddd88d9
vejmarie <jmverdun3@gmail.com>
2020-09-18 19:32:54 -0700

    Update opencamlib to work with python3.8

--- Formula/opencamlib.rb
+++ Formula/opencamlib.rb
@@ -1,47 +1,21 @@
 class Opencamlib < Formula
   desc "Computer Aided Manufacturing libraries"
   homepage "http://www.anderswallin.net/CAM/"
+  url "https://github.com/aewallin/opencamlib.git", :using => :git, :tag => "c3f3555270024104c51b27c33ecc7a293aae5dff"
   version "0.0.1" # TODO Specify a real version here - note usage below
-  url "https://github.com/aewallin/opencamlib.git",
-      :revision => "f8bd0a66ad4f1114a3caf84f430027c2da79c91d"
-  head "https://github.com/aewallin/opencamlib.git", :using => :git
+  head "https://github.com/aewallin/opencamlib.git", :using => :git, :tag => "c3f3555270024104c51b27c33ecc7a293aae5dff"
 
+  patch :p0 do
+    url "https://raw.githubusercontent.com/vejmarie/patches/master/OpenCAMlib/fix_mac.patch"
+    sha256 "e49a5a9ab1698019c53656f3ca6625db1b40012147998fd9b35f467917897295"
+  end
+
+  depends_on "python@3.8" => :build
   depends_on "cmake" => :build
-  depends_on "python@2" => :recommended
-
-  option "with-openmp", "Build with support for OpenMP parallel processing"
-  depends_on "llvm" => :build if build.with?("openmp")
 
   def install
-
-    if build.with? "openmp"
-      llvm_lib = Formula["llvm"].lib
-      llvm_inc = "#{llvm_lib}/clang/#{Formula["llvm"].version}" << "/include"
-    end
-
-    mkdir "build" do
-      cmake_args = std_cmake_args
-      if build.with? "openmp"
-        cmake_args << "-DCMAKE_C_COMPILER=#{Formula["llvm"].bin}/clang"
-        cmake_args << "-DCMAKE_CXX_COMPILER=#{Formula["llvm"].bin}/clang++"
-
-        cmake_args << "-DCMAKE_MODULE_LINKER_FLAGS=-undefined dynamic_lookup -L#{llvm_lib} -Wl,-rpath,#{llvm_lib}"
-
-        cmake_args << "-DCMAKE_C_FLAGS=-I#{llvm_inc}"
-        cmake_args << "-DCMAKE_CXX_FLAGS=-I#{llvm_inc} -std=c++11"
-      else
-        cmake_args << "-DUSE_OPENMP=0"
-      end
-
-      if build.with? "python"
-        cmake_args << "-DPYTHON_EXECUTABLE=#{Formula["python@2"].bin}/python2"
-      else
-        cmake_args << "-DBUILD_PY_LIB=0"
-      end
-
-      system "cmake", *cmake_args, ".."
+      args = std_cmake_args
+      system "cmake", *args, "-DVERSION_STRING=#{version}", "-DBUILD_TYPE=Release", "-DUSE_OPENMP=0", "-DBUILD_PY_LIB=ON","-DUSE_PY_3=TRUE", "-DPYTHON_VERSION_SUFFIX=3", "."
       system "make", "-j#{ENV.make_jobs}", "install"
-    end
   end
 end
-
