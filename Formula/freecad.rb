705fcada4b3d8a9be220681387c13fb828ece593
Jean-Marie Verdun <jmverdun3@gmail.com>
2020-09-20 14:05:11 -0700

    Add a specific patch to support proper compilation with boost 1.73.0

--- Formula/freecad.rb
+++ Formula/freecad.rb
@@ -19,6 +19,31 @@ class Freecad < Formula
   depends_on :macos => :catalina
   depends_on "freetype"
   depends_on "python3"
+  depends_on "boost"
+
+  if #{Formula["boost"].version}?("1.73.0")
+    md5 = `md5 -q #{Formula["boost"].prefix}/include/boost/geometry/index/detail/rtree/visitors/insert.hpp` ; result=$?.success?
+    if "#{md5}"=="bdffae5aee2ac909fe503f9afaae3ad9\n"
+       # The include file needs to be patched
+       # https://github.com/boostorg/geometry/commit/a74a2b5814a8753013a8966606b8472178fffd14
+       patch = "--- a/include/boost/geometry/index/detail/rtree/visitors/insert.hpp
++++ b/include/boost/geometry/index/detail/rtree/visitors/insert.hpp
+@@ -265,7 +265,7 @@ struct insert_traverse_data
+ // Default insert visitor
+ template <typename Element, typename MembersHolder>
+ class insert
+-    : MembersHolder::visitor
++    : public MembersHolder::visitor
+ {
+ protected:
+     typedef typename MembersHolder::box_type box_type;\n"
+
+      File.open("/tmp/include_insert_boost1.73.0.patch", "w") { |f| f.write "#{patch}\n" }
+      system "patch", "-p1", "/usr/local/Cellar/boost/1.73.0/include/boost/geometry/index/detail/rtree/visitors/insert.hpp" , "/tmp/include_insert_boost1.73.0.patch"
+      system "rm", "/tmp/include_insert_boost1.73.0.patch"
+    end
+  end
+
   depends_on "boost-python"
   depends_on "xerces-c"
   depends_on "qt"
