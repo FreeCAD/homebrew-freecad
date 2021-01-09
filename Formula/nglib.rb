9440c082cce96a324424482cf5e69b8c7c89cc7c
Andreas Sandberg <andreas@sandberg.uk>
2021-01-09 22:53:07 +0000

    nglib: Clean up formula
    Signed-off-by: Andreas Sandberg <andreas@sandberg.uk>

--- Formula/nglib.rb
+++ Formula/nglib.rb
@@ -1,8 +1,9 @@
 class Nglib < Formula
   desc "C++ Library of NETGEN's tetrahedral mesh generator"
   homepage "https://sourceforge.net/projects/netgen-mesher/"
-  url "https://github.com/NGSolve/netgen.git", using: :git, tag: "v6.2.2007"
-  version "v6.2.2007"
+  url "https://github.com/NGSolve/netgen.git",
+      tag:      "v6.2.2007",
+      revision: "9d71c172f33b62c18afa32fd90b836a3a7a0fc26"
   head "https://github.com/NGSolve/netgen.git"
 
   bottle do
@@ -12,23 +13,40 @@ class Nglib < Formula
   end
 
   depends_on "cmake" => :build
-
-  depends_on "#{@tap}/opencascade@7.5.0" => :required
+  depends_on "#{@tap}/opencascade@7.5.0"
 
   def install
     inreplace "CMakeLists.txt", "find_package(OpenCasCade REQUIRED)",
 "find_package(OpenCasCade REQUIRED HINTS \""+Formula["#{@tap}/opencascade@7.5.0"].opt_lib+"/cmake/opencascade\")\n   set(OCC_INCLUDE_DIR ${OpenCASCADE_INCLUDE_DIR})\n   message(${OpenCASCADE_INCLUDE_DIR})"
-    mkdir "Build" do
-      system "cmake", "-DUSE_PYTHON=OFF", "-DUSE_GUI=OFF", "-DUSE_OCC=ON",
-   '-DCMAKE_PREFIX_PATH="' + Formula["#{@tap}/opencascade@7.5.0"].opt_prefix + "/lib/cmake;", *std_cmake_args, ".."
-      system "make", "-j#{ENV.make_jobs}", "install"
+
+    args = std_cmake_args + %W[
+      -DUSE_PYTHON=OFF
+      -DUSE_GUI=OFF
+      -DUSE_OCC=ON
+      -DOCC_INCLUDE_DIR=#{Formula["#{@tap}/opencascade@7.5.0"].include}/opencascade
+    ]
+
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "make", "install"
     end
 
     # The nglib installer doesn't include some important headers by default.
     # This follows a pattern used on other platforms to make a set of sub
     # directories within include/ to contain these headers.
-    subdirs = %w[csg general geom2d gprim include interface
-                 linalg meshing occ stlgeom visualization]
+    subdirs = %w[
+      csg
+      general
+      geom2d
+      gprim
+      include
+      interface
+      linalg
+      meshing
+      occ
+      stlgeom
+      visualization
+    ]
     subdirs.each do |subdir|
       (include/"netgen"/subdir).mkpath
       (include/"netgen"/subdir).install Dir.glob("libsrc/#{subdir}/*.{h,hpp}")
