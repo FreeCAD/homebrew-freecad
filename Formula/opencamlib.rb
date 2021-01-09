84ba42add2f37c64d44064e23a51820a6b8a2a5c
Andreas Sandberg <andreas@sandberg.uk>
2021-01-09 16:30:41 +0000

    opencamlib: Clean up formula
    Signed-off-by: Andreas Sandberg <andreas@sandberg.uk>

--- Formula/opencamlib.rb
+++ Formula/opencamlib.rb
@@ -1,10 +1,17 @@
 class Opencamlib < Formula
   desc "Computer Aided Manufacturing libraries"
   homepage "http://www.anderswallin.net/CAM/"
-  url "https://github.com/aewallin/opencamlib.git", using: :git, revision: "c3f3555270024104c51b27c33ecc7a293aae5dff"
   version "0.0.1" # TODO: Specify a real version here - note usage below
-  head "https://github.com/aewallin/opencamlib.git", using:    :git,
-                                                     revision: "c3f3555270024104c51b27c33ecc7a293aae5dff"
+
+  stable do
+    url "https://github.com/aewallin/opencamlib.git",
+        revision: "c3f3555270024104c51b27c33ecc7a293aae5dff"
+
+    patch :p0 do
+      url "https://raw.githubusercontent.com/vejmarie/patches/f665f103e1e9d09eb080bfb9cddf36710891761d/OpenCAMlib/fix_mac.patch"
+      sha256 "e49a5a9ab1698019c53656f3ca6625db1b40012147998fd9b35f467917897295"
+    end
+  end
 
   bottle do
     root_url "https://justyour.parts:8080/freecad"
@@ -12,20 +19,32 @@ class Opencamlib < Formula
     sha256 cellar: :any, catalina: "695a0c707cc565aaa181049a2958e80fcaf21a76c573983e9d1314a19e90c8bd"
   end
 
-  depends_on "#{@tap}/boost-python3@1.75.0" => :build
-  depends_on "#{@tap}/boost@1.75.0" => :build
-  depends_on "#{@tap}/boost@1.75.0" => :build
-  depends_on "#{@tap}/python3.9" => :build
-  depends_on "cmake" => :build
-
-  patch :p0 do
-    url "https://raw.githubusercontent.com/vejmarie/patches/master/OpenCAMlib/fix_mac.patch"
-    sha256 "e49a5a9ab1698019c53656f3ca6625db1b40012147998fd9b35f467917897295"
+  head do
+    url "https://github.com/aewallin/opencamlib.git"
   end
 
+  depends_on "cmake" => :build
+  depends_on "#{@tap}/boost-python3@1.75.0"
+  depends_on "#{@tap}/boost@1.75.0"
+  depends_on "#{@tap}/python3.9"
+
   def install
-    args = std_cmake_args
-    system "cmake", *args, "-DVERSION_STRING=#{version}", "-DBUILD_TYPE=Release", "-DUSE_OPENMP=0", "-DBUILD_PY_LIB=ON", "-DUSE_PY_3=TRUE", "-DPYTHON_VERSION_SUFFIX=3", "-DCMAKE_PREFIX_PATH=" + Formula["#{@tap}/boost@1.75.0"].opt_prefix+ "/lib/cmake;" + Formula["#{@tap}/boost-python3@1.75.0"].opt_prefix+ "/lib/cmake;", "."
-    system "make", "-j#{ENV.make_jobs}", "install"
+    args = std_cmake_args + %W[
+      -DVERSION_STRING=#{version}
+      -DBUILD_TYPE=Release
+      -DUSE_OPENMP=0
+      -DBUILD_PY_LIB=ON
+      -DUSE_PY_3=TRUE
+      -DPYTHON_VERSION_SUFFIX=3
+    ]
+
+    args << "-DCMAKE_PREFIX_PATH=" + \
+            Formula["#{@tap}/boost@1.75.0"].opt_prefix+ "/lib/cmake;" + \
+            Formula["#{@tap}/boost-python3@1.75.0"].opt_prefix+ "/lib/cmake;"
+
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "make", "install"
+    end
   end
 end
