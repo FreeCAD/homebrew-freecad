8e3a1e65fea5c128b680fe81ecb3dfdaa4f0cfa5
chris <chris.r.jones.1983@gmail.com>
2021-08-27 11:53:36 -0500

    update pyside2 formula to use python@3.9.6 prefix

--- Formula/pyside2.rb
+++ Formula/pyside2.rb
@@ -35,7 +35,7 @@ class Pyside2 < Formula
 
     # Add out of tree build because one of its deps, shiboken, itself needs an
     # out of tree build in shiboken.rb.
-    pyhome = `#{Formula["#{@tap}/python3.9"].opt_bin}/python3.9-config --prefix`.chomp
+    pyhome = `#{Formula["#{@tap}/python@3.9.6"].opt_bin}/python3.9-config --prefix`.chomp
     py_library = "#{pyhome}/lib/libpython3.9.dylib"
     py_include = "#{pyhome}/include/python3.9"
 
