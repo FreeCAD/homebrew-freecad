d414089fc0460e734e09d1914b6a9730729e34c2
Chris <chris.r.jones.1983@gmail.com>
2021-08-31 15:50:29 -0600

    #hotfix cython, [no ci] fix python dep (#226)

--- Formula/cython@0.29.21.rb
+++ Formula/cython@0.29.21.rb
@@ -23,7 +23,7 @@ class CythonAT02921 < Formula
     Users are advised to use `pip` to install cython
   EOS
 
-  depends_on "freecad/freecad/python@3.9"
+  depends_on "freecad/freecad/python@3.9.6"
 
   def install
     xy = Language::Python.major_minor_version Formula["#{@tap}/python@3.9"].opt_bin/"python3"
