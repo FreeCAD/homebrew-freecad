c24c41b88bd1a0b04b9c2ed538f808e10687df4d
chris <chris.r.jones.1983@gmail.com>
2021-09-01 13:37:50 -0500

    #hotfix #exp, med-file, update test block to explicitly import `hdf5.h` as `hdf5@1.10` is keg-only

--- Formula/med-file.rb
+++ Formula/med-file.rb
@@ -11,8 +11,8 @@ class MedFile < Formula
     sha256 cellar: :any, mojave:    "112c796b6ae386478ee283bada3ce569d79638cd23abad655ca5f9c9d217b970"
   end
 
-  depends_on "./swig@4.0.2" => :build
   depends_on "cmake" => :build
+  depends_on "freecad/freecad/swig@4.0.2" => :build
   depends_on "gcc" => :build   # for gfortan
   depends_on "freecad/freecad/python@3.9.6"
   depends_on "hdf5@1.10"
@@ -39,7 +39,8 @@ class MedFile < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "-I#{include}", "-I#{Formula["hdf5@1.10"].opt_include}", "-L#{lib}", "-lmedC", "test.c"
+    # NOTE: hdf5@1.10 is keg-only, `-I#{Formula["hdf5@1.10].include` no good.
+    system ENV.cc, "-I#{include}", "-I/usr/local/opt/hdf5@1.10/include", "-L#{lib}", "-lmedC", "test.c"
     assert_equal version.to_s, shell_output("./a.out").chomp
   end
 end
