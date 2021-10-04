4c86e30dee9f3ab57a00a338cab5d2c6ce790ac9
Chris <chris.r.jones.1983@gmail.com>
2021-10-04 22:14:18 +0000

    shiboken2@5.15.2: add 5.15.2 bottle.

--- Formula/shiboken2@5.15.2.rb
+++ Formula/shiboken2@5.15.2.rb
@@ -4,6 +4,13 @@ class Shiboken2AT5152 < Formula
   url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.15.2-src/pyside-setup-opensource-src-5.15.2.tar.xz"
   sha256 "b306504b0b8037079a8eab772ee774b9e877a2d84bab2dbefbe4fa6f83941418"
 
+  bottle do
+    root_url "https://github.com/FreeCAD/homebrew-freecad/releases/download/shiboken2@5.15.2-5.15.2"
+    sha256 cellar: :any, big_sur:  "b8d2ad961130d7e8f6a838bc95b55c12d529befe6a0cd03aebef7792abd060e6"
+    sha256 cellar: :any, catalina: "43d87877ce4168d1d6c5574cf4dc26683845ed8fa74d5ca23aa9174dc167db8d"
+    sha256 cellar: :any, mojave:   "3ee35e362c5b373328c53f9a85927b1282e32ccf542343de359c3d96405ceef3"
+  end
+
   keg_only :versioned_formula # NOTE: will conflict with other shiboken2 installs
 
   depends_on "cmake" => :build
