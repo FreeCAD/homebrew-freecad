37477ad001b51341541480a131d466d44700abe5
Chris <chris.r.jones.1983@gmail.com>
2023-12-21 20:22:55 +0000

    fc_bundle: update 0.21.1 bottle.

--- Formula/fc_bundle.rb
+++ Formula/fc_bundle.rb
@@ -10,11 +10,12 @@ class FcBundle < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/freecad/freecad"
-    sha256 cellar: :any_skip_relocation, ventura:  "fcc2053794f3740c5c7c43f91a17476468d145fa96c194ae8ea0708c0653a2cb"
-    sha256 cellar: :any_skip_relocation, monterey: "2f0b06bb6813250a614166593611c981cb46d1a3409fd383e6e7a66f97d3e662"
-    sha256 cellar: :any_skip_relocation, big_sur:  "51a039df257ad819eb6a83be8e9f0f6913231f9a6dbce1472ee06b1762e943d9"
-    sha256 cellar: :any_skip_relocation, catalina: "3b794fd04de3f0a1fa649fd16dc10e9adbef1e4f2dffa30e66c16168ba5b6d5a"
-    sha256 cellar: :any_skip_relocation, mojave:   "818f00de497ec1a114d038e779c022065e516abcd89ae45c015ed8039d335068"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura:  "65a758b7418d67b5a1682cc43db7ff97b399d82dc10ca6483a24d229ee975ca0"
+    sha256 cellar: :any_skip_relocation, monterey: "9cc655ce372c9db6d63715ad9cbbb0a3186212379ee1f00ab181a241b4fc0250"
+    sha256 cellar: :any_skip_relocation, big_sur:  "3c7c115bbe7d30b2f69ee0898c4ffd9036b3db1d54aedd2f6d23f26f8b71dd33"
+    sha256 cellar: :any_skip_relocation, catalina: "e72656101232e918bd8896a9c417ce079fcdc3e8f04f7404d247ccea6e47a0fe"
+    sha256 cellar: :any_skip_relocation, mojave:   "c2d98149ee3424a44937836e065047d20ca5b327c8d1b988cd0feb30ba0b7d31"
   end
 
   depends_on "freecad/freecad/pyside2@5.15.11"
