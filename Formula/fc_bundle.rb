8004bab5a3eae9878b335b40dbd14950bd351977
chris <chris.r.jones.1983@gmail.com>
2023-12-28 09:20:05 -0600

    fc_bundle update .pth files
    Closes #470.
    
    Signed-off-by: Chris <chris.r.jones.1983@gmail.com>

--- Formula/fc_bundle.rb
+++ Formula/fc_bundle.rb
@@ -18,23 +18,28 @@ class FcBundle < Formula
     sha256 cellar: :any_skip_relocation, mojave:   "c2d98149ee3424a44937836e065047d20ca5b327c8d1b988cd0feb30ba0b7d31"
   end
 
-  depends_on "freecad/freecad/pyside2@5.15.11"
-  depends_on "freecad/freecad/shiboken2@5.15.11"
+  depends_on "freecad/freecad/coin3d_py310"
+  depends_on "freecad/freecad/pyside2@5.15.11_py310"
+  depends_on "freecad/freecad/shiboken2@5.15.11_py310"
 
   def install
     # explicitly set python version
-    pyver = "3.11"
+    pyver = "3.10"
 
     # Read the contents of the .pth file into a variable
     # shiboken2_pth_contents = \
     # File.read("#{Formula["shiboken2@5.15.11"].opt_prefix}/lib/python#{pyver}/site-packages/shiboken2.pth").strip
     pyside2_pth_contents =
-      File.read("#{Formula["pyside2@5.15.11"].opt_prefix}/lib/python#{pyver}/site-packages/pyside2.pth").strip
+      File.read("#{Formula["pyside2@5.15.11_py310"].opt_prefix}/lib/python#{pyver}/pyside2.pth").strip
 
-    site_packages = Language::Python.site_packages("python3.11")
+    coin3d_pivy_pth_contents =
+      File.read("#{Formula["coin3d_py310"].opt_prefix}/lib/python#{pyver}/coin3d_py310-pivy.pth").strip
+
+    site_packages = Language::Python.site_packages("python3.10")
     # {shiboken2_pth_contents}
     pth_contents = <<~EOS
       #{pyside2_pth_contents}
+      #{coin3d_pivy_pth_contents}
     EOS
     (prefix/site_packages/"freecad-py-modules.pth").write pth_contents
   end
@@ -42,7 +47,7 @@ class FcBundle < Formula
   test do
     # TODO: i think a more sane test is importing the python modules
     # Check if the expected site-packages file exists
-    site_packages_file = prefix/"lib/python3.11/site-packages/freecad-py-modules.pth"
+    site_packages_file = prefix/"lib/python3.10/site-packages/freecad-py-modules.pth"
     if site_packages_file.exist?
       puts "Test: OK - freecad-py-modules.pth file exists"
     else
