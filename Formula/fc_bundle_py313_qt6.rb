b992b65b7359ce99a23af8a5103bd11a4aa56e87
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-02-19 15:37:30 -0600

    rbme: update some values in new fc_bundle formula file NOT COMPLETE

--- Formula/fc_bundle_py313_qt6.rb
+++ Formula/fc_bundle_py313_qt6.rb
@@ -1,23 +1,16 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # SPDX-FileNotice: Part of the FreeCAD project.
 
-class FcBundlePy312Qt6 < Formula
+class FcBundlePy313Qt6 < Formula
   desc "Meta formula for bundling needed python modules"
   homepage "https://freecad.org/"
   # Dummy URL since no source is being downloaded
   url "file:///dev/null"
-  # this version works with the freecad v1rc2 release thus the 0.9.2 versioning
-  version "0.9.2"
+  # this version works with the freecad v1.0.2 release
+  version "1.0.2"
 
   # sha of file:///dev/null
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-
-  bottle do
-    root_url "https://ghcr.io/v2/freecad/freecad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "210c325b6bd30ce350f722ff7f818422413b23d15504a4b3e5a21fa2f3d0abf4"
-    sha256 cellar: :any_skip_relocation, ventura:      "b5dd87a97b865cb1435122209442131586f874d8b4484004638bf98fc28cbf4d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "533f5443466d7ab53744fee1371e52c740212b299215f92152ad65b729b0944e"
-  end
 
   depends_on "freecad/freecad/coin3d@4.0.3_py312_qt6"
   depends_on "freecad/freecad/med-file@4.1.1_py312"
@@ -30,14 +23,16 @@ class FcBundlePy312Qt6 < Formula
     sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   end
 
+  # TODO: need to add new dep lark as the latest freecad versions require it.
+
   def install
     # explicitly set python version
-    pyver = "3.12"
+    pyver = "3.13"
 
     venv_dir = libexec/"vendor"
 
     # Create a virtual environment
-    system "python3.12", "-m", "venv", venv_dir
+    system "python3.13", "-m", "venv", venv_dir
     venv_pip = venv_dir/"bin/pip"
 
     # Install the six module using pip in the virtual environment
