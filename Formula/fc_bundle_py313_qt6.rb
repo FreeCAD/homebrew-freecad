ae6536c0f450a74f8c3425ada4e8372907ddc1d6
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-03-01 14:51:26 -0600

    fc_bundle_py313_qt6: rebuild
    * fc_bundle_py313_qt6: add ifcopenshell as a dep
    * fc_bundle_py313_qt6: fix ifcopenshell install for arm64 linux
    * rbme: fc_bundle_py313_qt6: fix sha256sum mismatch
    * fc_bundle_py313_qt6: add zlib as a req dep for *nix
    * fc_bundle_py313_qt6: use zlib-ng-compat instead of zlib as a req dep for *nix
    
    Closes #771.
    
    Signed-off-by: Chris <chris.r.jones.1983@gmail.com>

--- Formula/fc_bundle_py313_qt6.rb
+++ Formula/fc_bundle_py313_qt6.rb
@@ -14,19 +14,52 @@ class FcBundlePy313Qt6 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/freecad/freecad"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aaf21d175d0174b5cb9622e6533b3adc230e1d4e37bdd3077774420bf26128b6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acc0e6630367bcc56bc4f175945cc05a515053fd202c7ae48c95c0d54cd0ef92"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "93412de408cbb9c582c8bc211cfce3e295bbe00ff62cdd38027f0aa42162acc8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e2efcb75016b3d97029ff3713d3bead5f2bfc72c6fb7e3e3b8046726a9b7dc3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ed009d05a9ea96f4fc3159d01d47b1952f72e7e3e40eb7847249e9e7af0e616d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf5d89e8765fe5891672b0466363afdf3c49026506cd5ccc404b72dcc9f710d1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c11ad393f7ec92ed17bf5a599063fce0863abb734b982d9a810cd7072a61a4c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ed5548d18df013b0900ff09eb89fb66e8b9c7fcf368118df2f1bc82f0bf76d9"
   end
 
+  depends_on "patchelf" => :build
+  depends_on "pkgconf" => :build
   depends_on "freecad/freecad/coin3d@4.0.7_py313_qt6"
   depends_on "freecad/freecad/med-file@5.0.0_py313"
   depends_on "freecad/freecad/pyside6_py313" # pyside includes the shiboken module as well
   depends_on "freecad/freecad/vtk@9.5.2_py313"
+  depends_on "geos"
+  depends_on "libyaml"
   depends_on "numpy"
-  depends_on "pybind11"
+  depends_on "pybind11" # reqd by pyyaml
+  depends_on "webp" if OS.linux?
+  depends_on "zlib-ng-compat" if OS.linux?
+
+  # NOTE: ipatch, https://docs.ifcopenshell.org/ifcopenshell-python/installation.html#zip-packages
+  resource "ifcopenshell" do
+    if OS.mac? && Hardware::CPU.arm?
+      url "https://github.com/IfcOpenShell/IfcOpenShell/releases/download/ifcopenshell-python-0.8.4/ifcopenshell-python-0.8.4-py313-macosm164.zip"
+      sha256 "a9015197e75e025ce8a35ab9f40e23ab58e2cf414db176a5ed7873bb82d5f3d0"
+    elsif OS.mac?
+      url "https://github.com/IfcOpenShell/IfcOpenShell/releases/download/ifcopenshell-python-0.8.4/ifcopenshell-python-0.8.4-py313-macos64.zip"
+      sha256 "7edd7d0c5d234ae74934f4a9c81c9d2a02d376a42ebbe52a4a50dba62031c24a"
+    elsif OS.linux? && Hardware::CPU.arm?
+      url "https://files.pythonhosted.org/packages/f6/6f/2f8c4f64796c993f7014c77f8d38ddc0e073cac4ad3efab64231cdfbd655/ifcopenshell-0.8.4.post1-py313-none-manylinux_2_31_aarch64.whl"
+      sha256 "ae1c75b42768db0c40d3d7de0a17f1836488591908f45c9dd3fa5b993ee2b61b"
+    elsif OS.linux?
+      url "https://github.com/IfcOpenShell/IfcOpenShell/releases/download/ifcopenshell-python-0.8.4/ifcopenshell-python-0.8.4-py313-linux64.zip"
+      sha256 "1d3e49c65636f5d46a4c6825142ffd3b97a6fbcedaeb24301166cd256103f24c"
+    end
+  end
+
+  # NOTE: newer versions of the BIM wb require lark
+  resource "lark" do
+    url "https://files.pythonhosted.org/packages/da/34/28fff3ab31ccff1fd4f6c7c7b0ceb2b6968d8ea4950663eadcb5720591a0/lark-1.3.1.tar.gz"
+    sha256 "b426a7a6d6d53189d318f2b6236ab5d6429eaf09259f1ca33eb716eed10d2905"
+  end
+
+  resource "ply" do
+    url "https://files.pythonhosted.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da/ply-3.11.tar.gz"
+    sha256 "00c7c1aaa88358b9c765b6d3000c6eec0ba42abca5351b095321aef446081da3"
+  end
 
   # NOTE: it appears it has been several years since the six pypi package has been updated
   resource "six" do
@@ -34,10 +67,9 @@ class FcBundlePy313Qt6 < Formula
     sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   end
 
-  # NOTE: newer versions of the BIM wb require lark
-  resource "lark" do
-    url "https://files.pythonhosted.org/packages/da/34/28fff3ab31ccff1fd4f6c7c7b0ceb2b6968d8ea4950663eadcb5720591a0/lark-1.3.1.tar.gz"
-    sha256 "b426a7a6d6d53189d318f2b6236ab5d6429eaf09259f1ca33eb716eed10d2905"
+  resource "shapely" do
+    url "https://files.pythonhosted.org/packages/4d/bc/0989043118a27cccb4e906a46b7565ce36ca7b57f5a18b78f4f1b0f72d9d/shapely-2.1.2.tar.gz"
+    sha256 "2ed4ecb28320a433db18a5bf029986aa8afcfd740745e78847e330d5d94922a9"
   end
 
   # TODO: still probably need to add the pynastran to make the test suite happy
@@ -54,11 +86,30 @@ class FcBundlePy313Qt6 < Formula
 
     # Install the six module using pip in the virtual environment
     # certain freecad workbenches require the python six module
-    # setup and install both six and lark
-    resources.each do |r|
-      r.stage do
+    # setup and install lark ply six
+    %w[lark ply six].each do |pkg|
+      resource(pkg).stage do
         system venv_pip, "install", "."
       end
+    end
+
+    resource("ifcopenshell").stage do
+      if OS.linux? && Hardware::CPU.arm?
+        # .whl is just a zip - extract and copy the package dir
+        system "unzip", "-o", Dir["*.whl"].first, "-d", "."
+        (libexec/"vendor/lib/python3.13/site-packages").install "ifcopenshell"
+      else
+        # github .zip's should have the correct dir layout
+        site_packages = venv_dir/"lib/python#{pyver}/site-packages"
+        (site_packages/"ifcopenshell").install Dir["*"]
+      end
+    end
+
+    # ifcopenshell dep
+    resource("shapely").stage do
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula["geos"].opt_lib/"pkgconfig"
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula["freecad/freecad/numpy@2.1.1_py312"].opt_lib/"pkgconfig"
+      system venv_pip, "install", "."
     end
 
     # Example: Read the contents of the .pth file into a variable
