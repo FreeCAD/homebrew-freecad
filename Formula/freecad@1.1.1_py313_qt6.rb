1d28dfb83fcd5fb3e1e3b1302626fbf9d449dce2
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-06-22 12:05:52 -0500

    freecad@1.1.1_py313_qt6: update test block to specify path for user.cfg and system.cfg

--- Formula/freecad@1.1.1_py313_qt6.rb
+++ Formula/freecad@1.1.1_py313_qt6.rb
@@ -526,6 +526,11 @@ class FreecadAT111Py313Qt6 < Formula
 
   test do
     freecadcmd = OS.mac? ? prefix/"MacOS/FreeCADCmd" : bin/"FreeCADCmd"
+    with_env(HOME: testpath, TMPDIR: testpath) do
+      system freecadcmd, "-u", testpath/"user.cfg",
+        "-s", testpath/"system.cfg",
+        "-t", "0"
+    end
     system freecadcmd, "-t", "0"
   end
 end
